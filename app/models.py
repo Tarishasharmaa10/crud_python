2eba1a7653cf60f16149a8614b3003fce63e679b
Tarisha Sharma <tarisha2024@gmail.com>
2024-09-05 18:06:44 +0530

    Changes

--- app/models.py
+++ app/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class student(models.Model):
-    name=models.CharField(max_length=25,blqnk=False,null=False)
+class Student(models.Model):
+    name=models.CharField(max_length=25,blank=False,null=False)
     email=models.EmailField()
     age=models.IntegerField()
-    name=models.CharField(max_length=25,blqnk=False,null=False)
+    gender=models.CharField(max_length=25,blank=False,null=False)
+
+    def __str__(self) :
+        return self.name
